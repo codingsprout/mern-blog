eb361efb9c4552fa224da3a11c95b89e02196ddc
codingsprout <iqsymphic@gmail.com>
2020-11-24 18:20:47 -0500

    update/delete category
    php blegh

---
+++ back-end/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+const blogRoutes = require('./routes/blog');
+
+//middleware
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
+
+mongoose
+  .connect(process.env.DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Database is connected!'));
+
+// routes
+
+app.use(blogRoutes);
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
